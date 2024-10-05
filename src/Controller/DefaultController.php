903ec6e17419ef4b8ec7dce999f07a6ae2ec250a
andressanzr <43200496+andrewpolski@users.noreply.github.com>
2024-10-05 21:56:49 +0200

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController extends AbstractController
+{
+    
+    #[Route('/')]
+    public function defaultAction(): Response
+    {
+        return $this->render("home.html.twig");
+    }
+}
